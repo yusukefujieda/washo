09962d0b5af54704b273dcd3738c9fe7db1a2c59
藤枝悠介 <yusuke.fujieda@gamil.com>
2016-02-16 15:58:50 +0900

    migrationファイル編集

--- db/migrate/20160216040600_create_guides.rb
+++ db/migrate/20160216040600_create_guides.rb
@@ -1,8 +1,9 @@
 class CreateGuides < ActiveRecord::Migration
   def change
     create_table :guides do |t|
-      t.integer :shop
-      t.string :name
+      t.integer :shop_id
+      t.integer :user_id
+      t.integer :category
 
       t.timestamps
     end
